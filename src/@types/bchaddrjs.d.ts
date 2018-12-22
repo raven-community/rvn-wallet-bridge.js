b61b4e0b87fa4a847e4fb6a074d8079ac0d40b84
MSFTserver <sawyerlemmo@gmail.com>
2018-12-22 11:47:33 -0800

    remove bitcoincash files

--- src/@types/bchaddrjs.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "rvnaddrjs" {
-  function isRvn2Address(address: string): boolean
-  function isP2SHAddress(address: string): boolean
-  function toRvn2Address(address: string): string
-  function toLegacyAddress(address: string): string
-}
